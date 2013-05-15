82d46a871a43bb6e4b5b7ae151a0ec26ae503c41
Junyi BEI <beijunyi@gmail.com>
2013-05-15 01:06:05 +0100

    fking commit message

--- src/main/java/im/grusis/mkb/Unserializer.java
+++ src/main/java/im/grusis/mkb/Unserializer.java
@@ -1,6 +1,6 @@
 package im.grusis.mkb;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * User: beij
@@ -37,6 +37,78 @@ public class Unserializer {
     int lengthBreak = value.indexOf(":", 1);
     int length = Integer.valueOf(value.substring(1, lengthBreak));
     return value.substring(lengthBreak + 1, lengthBreak + 1 + length);
+  }
+
+  private int findEnd(String value, int startIndex) {
+    int length = value.length();
+    int openBrackets = 0;
+    for(int i = startIndex; i < length; i++) {
+      if(value.charAt(i) == '{') {
+        openBrackets++;
+        continue;
+      }
+      if(value.charAt(i) == '}') {
+        openBrackets--;
+        if(openBrackets == 0) return i + 1;
+        continue;
+      }
+      if(openBrackets == 0 && value.charAt(i) == ';') return i + 1;
+    }
+    return -1;
+  }
+
+  public Map<String, Object> unserializeMap(String value) {
+    Map<String, Object> result = new LinkedHashMap<String, Object>();
+    int sizeBreak = value.indexOf(":", 1);
+    int size = Integer.valueOf(value.substring(1, sizeBreak));
+    int start = sizeBreak + 2, end;
+    String key;
+    Object object;
+    for(int i = 0; i < size; i++) {
+      end = value.indexOf(";", start);
+      key = unserializeString(value.substring(start, end));
+      start = end;
+      end = findEnd(value, start);
+      object = unserialize(value.substring(start, end));
+      result.put(key, object);
+    }
+    return result;
+  }
+
+  public Date unserializeDate(String value) {
+    Map<String, Object> map = unserializeMap(value);
+    Calendar calendar = Calendar.getInstance();
+    calendar.set(Calendar.YEAR, (Integer) map.get("year"));
+    calendar.set(Calendar.MONTH, (Integer) map.get("month") - 1);
+    calendar.set(Calendar.DAY_OF_MONTH, (Integer) map.get("day"));
+    calendar.set(Calendar.HOUR_OF_DAY, (Integer) map.get("hour"));
+    calendar.set(Calendar.MINUTE, (Integer) map.get("minute"));
+    calendar.set(Calendar.SECOND, (Integer) map.get("second"));
+    calendar.set(Calendar.MILLISECOND, (Integer) map.get("millisecond"));
+    return calendar.getTime();
+  }
+
+  public Object unserialize(String value) {
+    char head = value.charAt(0);
+    int start = 1;
+    int end = findEnd(value, start);
+    String data = value.substring(start, end);
+    switch(head) {
+      case 'N':
+        return unserializeNull();
+      case 'b':
+        return unserializeBoolean(data);
+      case 'i':
+        return unserializeInteger(data);
+      case 'd':
+        return unserializeDouble(data);
+      case 's':
+        return unserializeString(data);
+      case 'a':
+        return unserializeMap(data);
+      default:
+        return null;
+    }
   }
 
 }
