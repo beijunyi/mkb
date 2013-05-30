748a00d7c863760ad0bd8a5acaeff209653f3af3
Junyi BEI <junyi.bei@formicary.net>
2013-05-30 11:57:03 +0100

    commit

---
+++ src/test/java/im/grusis/mkb/emulator/registerhelperTest.java
@@ -0,0 +1,15 @@
+package im.grusis.mkb.emulator;
+
+/**
+ * User: Mothership
+ * Date: 13-5-23
+ * Time: 下午8:25
+ */
+public class registerhelperTest {
+
+//  @Test
+//  public void overallTest() {
+//    ServerInformationResponse si = PassportHelper.getServerInformation();
+//    Assert.assertFalse(si.getReturnObjs().getGAME_SERVER().isEmpty());
+//  }
+}
