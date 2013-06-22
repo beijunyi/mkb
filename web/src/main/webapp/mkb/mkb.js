436140eedccdef293cd3a4938dc3158b45ac2072
Junyi BEI <beijunyi@gmail.com>
2013-06-22 16:43:33 +0100

    milestone 19

--- web/src/main/webapp/mkb/mkb.js
+++ web/src/main/webapp/mkb/mkb.js
@@ -1,28 +1,28 @@
 var app = angular.module('mkb', ['ngResource']);
 
-app.config(function($routeProvider) {
+app.config(function($routeProvider, $locationProvider) {
+  $locationProvider
+    .html5Mode(false);
   $routeProvider
     .when('/account', {
-      title: 'Account',
       templateUrl: 'mkb/views/account.html',
       controller: 'AccountCtrl'
     })
     .when('/manual', {
-      title: 'Manual Access',
       templateUrl: 'mkb/views/manual.html',
       controller: 'ManualAccessCtrl'
     })
     .when('/auto', {
-      title: 'Automated Production',
       templateUrl: 'mkb/views/auto.html',
       controller: 'AutomatedProductionCtrl'
     })
     .when('/stats', {
-      title: 'Statistics',
       templateUrl: 'mkb/views/stats.html',
       controller: 'AutomatedProductionCtrl'
     })
     .otherwise({
       redirectTo: '/account'
     })
+
+
 });
